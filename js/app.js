08391f9386ea4a644a894e6821a72011687d1eb1
velvetcerise <velvet.cerise@hotmail.com>
2018-01-30 22:31:55 -0600

    actualización

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
-//views for followers, followings and spoons
+/ Initialize Firebase
+var config = {
+   apiKey: "AIzaSyBK_CUraaMhvELqZS1ev2zE2dChioqXyyg",
+   authDomain: "spoon-ea8a3.firebaseapp.com",
+   databaseURL: "https://spoon-ea8a3.firebaseio.com",
+   projectId: "spoon-ea8a3",
+   storageBucket: "",
+   messagingSenderId: "254543320278"
+};
+firebase.initializeApp(config);
 
-function view(option) {
-    var i;
-    var x = document.getElementsByClassName("tablas");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    document.getElementById(view).style.display = "block";
-}
+// elementos
+
+var txtEmail = document.getElementById("txt-email");
+var txtPassword = document.getElementById("txt-password");
+var login = document.getElementById("login");
+var signUp = document.getElementById("sign-up");
+
+// añadir eventos
+login.addEventListener("click", e => {
+   // obtener email y contraseña
+   var email = txtEmail.value;
+   var pass = txtPassword.value;
+   var auth = firebase.auth();
+   // autenticar
+   var declaration = auth.signInWithEmailAndPassword(email, pass);
+   declaration.catch(e => console.log(e.message));
+});
+
+// registro
+signUp.addEventListener("click", e => {
+   var email = txtEmail.value;
+   var pass = txtPassword.value;
+   var auth = firebase.auth();
+   // autenticar
+   var declaration = auth.createUserWithEmailAndPassword(email, pass);
+   declaration.catch(e => console.log(e.message));
+});
+
+// agregar un usuario
+firebase.auth().onAuthStateChanged(firebaseUser => {
+   if (firebase) {
+       console.log(firebaseUser);
+   } else{
+       console.log("no registrado");
+   }
+});
